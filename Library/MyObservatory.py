a8ea4f8cb2c810f889711a55dad79730135f80c1
Gary Lien <gary.lien@hp.com>
2022-03-13 16:51:35 +0800

    add check_exists_by_id

--- Library/MyObservatory.py
+++ Library/MyObservatory.py
@@ -1,24 +1,35 @@
 from appium import webdriver
-from appium.webdriver.common.touch_action import TouchAction
+from appium.webdriver.common.appiumby import AppiumBy
 import time
 
 
 class App():
     def __init__(self):
         desired_caps = {}
-        # 设备系统
         desired_caps['platformName'] = 'Android'
-        # 设备系统版本号
         desired_caps['platformVersion'] = '7.1.2'
-        # 设备名称
         desired_caps['deviceName'] = '127.0.0.1:62001'
-
-        # 应用的包名
         desired_caps['appPackage'] = 'hko.MyObservatory_v1_0'
         desired_caps['appActivity'] = 'hko.homepage.Homepage2Activity'
 
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)  # 地址为appium地址
-        time.sleep(8)
+        time.sleep(5)
+
+        try:
+            self.driver.find_element(AppiumBy.ID, "hko.MyObservatory_v1_0:id/btn_friendly_reminder_skip").click()
+            print(123132)
+        except Exception:
+            pass
+
+    def check_exists_by_id(self, search_id):
+        try:
+            # self.driver.find_element_by_id(search_id)
+            self.driver.find_element(AppiumBy.ID, search_id)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
 
 
 def main():
